bd9a64e8552f2986d6713d501ef5b301f19ebd8e
Htet Aung <htetaung20025q@example.com>
2026-08-07 21:13:22 +0630

    db

--- config/database.py
+++ config/database.py
@@ -1,7 +1,11 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql+asyncpg://htetagg:password123@localhost/finalproject"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", 
+    "postgresql+asyncpg://htetagg:password123@localhost/finalproject"
+)
 
 engine = create_async_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(
